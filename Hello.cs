a22955c99295ab4ffae3c31c493aa00b2c5dbb89
BenjaminStimec <benjamin.stimec@student.um.si>
2020-11-20 00:07:25 +0100

    Hello.cs veja

--- Hello.cs
+++ Hello.cs
@@ -1,17 +1,9 @@
 public class Hello
 {
-<<<<<<< HEAD
 	public static void Main()
 	{
 		System.Console.WriteLine("Hello, World!");
+		System.Console.WriteLine("Hello, GitHub!");
 		System.Console.WriteLine("Hello, Local!");
 	}
 }
-=======
-   public static void Main()
-   {
-	System.Console.WriteLine("Hello, World!");
-	System.Console.WriteLine("Hello, GitHub!");
-   }
-}
->>>>>>> 8a741e3542d9e0a7702dbbffc499167baf62f07d
